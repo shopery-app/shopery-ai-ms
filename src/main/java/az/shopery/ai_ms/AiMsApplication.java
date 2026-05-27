e68f6001ce466b42b2e186609746e9fac7619d13
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-05-27 13:27:43 +0400

    project setup

---
+++ src/main/java/az/shopery/ai_ms/AiMsApplication.java
@@ -0,0 +1,13 @@
+package az.shopery.ai_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/shopery/ai_ms/AiMsApplicationTests.java
@@ -0,0 +1,13 @@
+package az.shopery.ai_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
